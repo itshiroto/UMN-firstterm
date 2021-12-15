f38c875ec755c77c6757a2f37de134f659ecdf6f
itshiroto <rivowowor7@gmail.com>
2021-12-15 08:44:33 +0800

    Scripted auto-commit on change (2021-12-15 08:44:33) by gitwatch.sh

--- IF130/UAS/nomor2.c
+++ IF130/UAS/nomor2.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
+void printMenu() {
+    printf("1. Rectangle");
+    printf("2. Triangle");
+    printf("3. Circle");
+}
+
 int main() {
-    printf("Hello World\n");
+    int menu;
+    printf("Pilih Menu");
+    scanf("%i", &menu);
     return 0;
 }
