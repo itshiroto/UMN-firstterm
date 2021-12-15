48d5daffdbc43f39d4efca13a32d280a45792cb9
itshiroto <rivowowor7@gmail.com>
2021-12-15 08:11:19 +0800

    Scripted auto-commit on change (2021-12-15 08:11:19) by gitwatch.sh

--- IF130/UAS/nomor1.c
+++ IF130/UAS/nomor1.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 
+int getLen(char *input) {
+    int count;
+    while (input[count] != '\0') {
+        count++;
+    }
+    return count;
+
+
 int main() {
     char *password[64];
     printf("Input Password: ");
     scanf("%s", &password);
-    printf("Password: %s", password);
+    // printf("Password: %s", password);
+    printf("%i", getLen(password));
+    // for (int i = 0; i < 
     return 0;
 }
