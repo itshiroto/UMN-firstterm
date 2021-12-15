52e73506b7c7be824923d5c2124591f48a16fe37
itshiroto <rivowowor7@gmail.com>
2021-12-15 09:49:35 +0800

    Scripted auto-commit on change (2021-12-15 09:49:35) by gitwatch.sh

---
+++ IF130/UAS/rivo-uas/nomor1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int checkPass(char password[]) {
+    int count = 0;
+    int number = 0;
+    int lower = 0;
+    int upper = 0;
+    int symbol = 0;
+    for(count = 0; password[count] != '\0'; count++) {
+        if (password[count] == '\0') break;
+        int isSymbol = 0;
+        password[count] >= '0' && password[count] <= '9' ? number++ : isSymbol++;
+        password[count] >= 'A' && password[count] <= 'Z' ? upper++ : isSymbol++;
+        password[count] >= 'a' && password[count] <= 'z' ? lower++ : isSymbol++;
+        if (isSymbol >= 3) symbol++;
+    }
+    // printf("%i %i %i %i %i", number, lower, upper, symbol, count);
+    if (count < 8 || number < 1 || lower < 1 || upper < 1 || symbol > 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    char password[64];
+    printf("Input Password: ");
+    scanf("%s", password);
+    checkPass(password) ? printf("Invalid") : printf("Valid");
+    return 0;
+}
+
---
+++ IF130/UAS/rivo-uas/nomor2.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+
+void printMenu() {
+    printf("\n");
+    printf("1. Rectangle\n");
+    printf("2. Triangle\n");
+    printf("3. Circle\n");
+    printf("4. Exit\n");
+}
+
+int isDigit(char *input){
+    int i = 0;
+    while (input[i] != '\0') {
+      // check if element i of a string is a character based on ASCII code.
+      if (!(input[i] >= '0' && input[i] <= '9')) return 1; 
+      i++;
+    }
+    return 0;
+}
+
+int convertToInt(char *str){
+  int i = 0, result = 0;
+  while (str[i] != '\0') {
+    // Each i element in str variable is subtracted by 48 ('0' character on ASCII)
+    // to get the real number and then added to result variable.
+    result = result * 10 + str[i] - '0'; 
+    i++;
+  }
+  return result;
+}
+
+void printResult(float kel, float sisi) {
+    printf("\n");
+    printf("Keliling = %.2f\n", kel);
+    printf("Sisi = %.2f\n", sisi);
+}
+
+void rectangle() {
+    char pchar[10], lchar[10];
+    float keliling, luas;
+
+    printf("Input Panjang: ");
+    scanf("%s", &pchar);
+    printf("Input Lebar: ");
+    scanf("%s", &lchar);
+    if ((isDigit(pchar) == 1 || isDigit(lchar) == 1)) {
+        printf("Input bukan angka\n");
+        return;
+    }
+    int panjang = convertToInt(pchar);
+    int lebar = convertToInt(lchar);
+
+    keliling = 2 * (panjang * lebar);
+    luas = panjang * lebar;
+    printResult(keliling, luas);
+}
+
+void triangle() {
+    char schar[10];
+    float keliling, luas;
+
+    printf("Input sisi: ");
+    scanf("%s", &schar);
+
+    if ((isDigit(schar) == 1)) {
+        printf("Input bukan angka\n");
+        return;
+    }
+    int sisi = convertToInt(schar);
+
+    keliling = 3 * sisi;
+    luas = (sisi * 2) / 2;
+
+    printResult(keliling, luas);
+}
+
+void circle() {
+    char jchar[10];
+    const float pi = 3.14;
+    float keliling, luas;
+
+    printf("Input jari-jari: ");
+    scanf("%s", &jchar);
+
+    if ((isDigit(jchar) == 1)) {
+        printf("Input bukan angka\n");
+        return;
+    }
+
+    int jari = convertToInt(jchar);
+
+    keliling = 2 * pi * jari;
+    luas = pi * jari * jari;
+
+    printResult(keliling, luas);
+}
+
+int inputMenu() {
+    int menu;
+    printf("Pilih Menu: ");
+    scanf("%i%*c", &menu);
+    return menu;
+}
+
+int main() {
+    int keepGoing = 1;
+    while (keepGoing != 0) {
+        printMenu();
+        switch(inputMenu()) {
+            case 1: rectangle(); break;
+            case 2: triangle(); break;
+            case 3: circle(); break;
+            case 4: keepGoing = 0;
+            default: printf("Menu Tidak Ditemukan"); break;
+        }
+    }
+    return 0;
+}
