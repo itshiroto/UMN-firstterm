b84784c840cfdb587dcdf62524a6ebd341b7689b
itshiroto <rivowowor7@gmail.com>
2021-12-15 08:30:58 +0800

    Scripted auto-commit on change (2021-12-15 08:30:58) by gitwatch.sh

--- IF130/UAS/nomor1.c
+++ IF130/UAS/nomor1.c
@@ -11,11 +11,12 @@ int main() {
     int upper = 0;
     int symbol = 0;
     for(int count = 0; password != '\0'; count++) {
+        if password[count] = '\0' return
         int isSymbol = 0;
         password[count] >= '0' && password[count] <= '9' ? number++ : isSymbol++;
         password[count] >= 'A' && password[count] <= 'Z' ? upper++ : isSymbol++;
         password[count] >= 'a' && password[count] <= 'z' ? lower++ : isSymbol++;
-        isSymbol != 0 ? symbol++ : symbol;
+        isSymbol != 0 ? symbol++ : ;
     }
     printf("%i %i %i %i %i", number, lower, upper, symbol, count);
     return 0;
